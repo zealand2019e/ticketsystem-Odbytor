c1e9add42e0edc6816110badd76476a8ecc129c7
Bartosz <DonMarchewa@gmail.com>
2019-09-18 13:46:24 +0200

    Task 3

--- Testing/UnitTest1.cs
+++ Testing/UnitTest1.cs
@@ -37,5 +37,34 @@ namespace Testing
             Assert.AreEqual(type, "Car");
         }
 
+        /// <summary>
+        /// Test class to test if Price methods returns correct price.
+        /// </summary>
+        [TestMethod]
+        public void TestPrice2()
+        {
+            //Arrange
+            double price = 0;
+            MC tmc = new MC();
+            //Act
+            price = tmc.Price();
+            //Assert
+            Assert.AreEqual(price, 125);
+        }
+
+        /// <summary>
+        /// Test method to test if Price method returns "Car" string.
+        /// </summary>
+        [TestMethod]
+        public void TestVehicleType2()
+        {
+            //Arrange
+            string type;
+            MC tcar = new MC();
+            //Act
+            type = tcar.Vehicle();
+            //Assert
+            Assert.AreEqual(type, "MC");
+        }
     }
 }
---
+++ TicketLibrary/MC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketLibrary
+{
+   public class MC
+    {
+        public string Licensplate { get; set; }
+        public DateTime date { get; set; }
+
+        public double Price()
+        {
+            return 125;
+        }
+
+        public string Vehicle()
+        {
+            return "MC";
+        }
+    }
+}
